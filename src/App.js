dde663e6840e63991a349400bec2a60c3a0f4432
Teja Pullabhotla <teja.pullabhotla@jll.com>
2023-12-17 00:26:08 -0600

    Initial Commit other folders

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import 'rsuite/dist/rsuite.min.css';
+import './App.css';
+import LoginScreen from "./components/LoginScreen/LoginScreen";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Dashboard from "./components/Dashboard/Dashboard";
+import PatientRecordsPage from "./components/PatientRecordsPage/PatientRecordsPage";
+
+function App() {
+  return (
+    <div className="App">
+        <Router>
+            <Switch>
+                <Route path={"/login"} exact={true} component={LoginScreen} />
+                <Route path={"/patientrecords"} exact={true} component={PatientRecordsPage} />
+                <Route path={"/home"} exact={true} component={Dashboard} />
+            </Switch>
+        </Router>
+    </div>
+  );
+}
+
+export default App;
